4f79700abb02a0d6f733ac98eb69091eb24ed7ad
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-13 16:55:04 -0400

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Home from "./screens/Home"
+import PostDetails from "./screens/PostDetails"
+import PostCreate from "./screens/PostCreate"
+import PostEdit from "./screens/PostEdit"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/post/:id" exact component={PostDetails} />
+        <Route path="/post/create" exact component={PostCreate} />
+        <Route path="/post/:id/edit" exact component={PostEdit} />
+      </Switch>
     </div>
   );
 }
