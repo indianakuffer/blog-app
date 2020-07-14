7e7c817bc7d0d9f057baabf976fb2e94028513bb
zahrakhan <zahrakhadijha@gmail.com>
2020-07-13 22:24:39 -0400

    add nav bar

---
+++ client/src/components/shared/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Nav = () => {
+
+  return (
+    <nav>
+      <div className="nav">
+        <NavLink className="logo" to="/">Blog App</NavLink>
+        <div className="links">
+          <NavLink className="link" to="/posts">Blog Posts</NavLink>
+        </div>
+      </div>
+    </nav>
+  )
+}
